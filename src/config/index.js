9da3c6b6355741a70153e4618f859a0765af44e9
angyRincon <grincon@celerik.com>
2022-12-16 10:20:33 -0500

    Configuring db credentials

--- src/config/index.js
+++ src/config/index.js
@@ -4,6 +4,10 @@ dotenvConfig();
 
 export const config = {
     environment: {
-        SECRET: process.env.NODE_JWT_SECRET
+        SECRET: process.env.NODE_JWT_SECRET,
+        MONGOUSER: process.env.NODE_MONGO_USER,
+        MONGOPASSWORD: process.env.NODE_MONGO_PASSWORD,
+        MONGOHOST: process.env.NODE_MONGO_HOST,
+        MONGOPORT: process.env.NODE_MONGO_PORT,
     }
 }
--- src/database.js
+++ src/database.js
@@ -1,12 +1,16 @@
 //@packages
 import mongoose from "mongoose";
 
+import { config } from "./config/index.js";
 
+const { MONGOUSER, MONGOPASSWORD, MONGOHOST, MONGOPORT } = config.environment;
+
+const uri = `mongodb://${MONGOUSER}:${MONGOPASSWORD}@${MONGOHOST}:${MONGOPORT}`
 
 export const connectDataBase = async () => {
     try {
         mongoose.set('strictQuery', false);
-        const db = await mongoose.connect('mongodb://localhost/book-store', {
+        const db = await mongoose.connect(uri, {
             useNewUrlParser: true,
         })
         console.log('DB connected at', db.connection.name)
